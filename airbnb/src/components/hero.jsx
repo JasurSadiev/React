9d0886cf21b3cc235d99c371946db0b9da8414cf
JasurSadiev <goldenwashington159@gmail.com>
2022-04-19 07:37:43 -0400

    Made changes in style in footer.

--- airbnb/src/components/hero.jsx
+++ airbnb/src/components/hero.jsx
@@ -3,7 +3,7 @@ import hero from '../images/photo-grid.png';
 
 const Hero = () => {
   return (
-    <section>
+    <section className="hero__section">
       <img className="hero__img" src={hero} alt="hero" />
       <h1 className="hero__title">Online Experiences</h1>
       <p className="hero__content">Join unique interactive activities led by one-of-a-kind hosts—all without leaving home.</p>
